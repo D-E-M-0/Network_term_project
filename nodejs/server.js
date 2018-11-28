bbc2dee1c7b2848662b5200b02b3d073f241a879
Monocono <39505929+Monocono@users.noreply.github.com>
2018-11-28 22:20:37 +0900

    Delete server.js

--- nodejs/server.js
+++
@@ -1,9 +0,0 @@
-var port = process.env.PORT || 1347;
-var io = require('socket.io')(port);
-var shortid = require('shortid');
-
-console.log("server is running on port "+port);
-io.on('connection',function(socket){
-
-  var
-})
